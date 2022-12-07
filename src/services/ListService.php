856779d4452b5f6ffe3009cbcb6f04d1e7aac533
Sergey Golyshev <s.golyshev@globus-ltd.com>
2022-12-07 11:48:22 +0300

    refactoring getAllItems service

--- src/services/ListService.php
+++ src/services/ListService.php
@@ -13,34 +13,41 @@ class ListService
     }
 
     /**
-     * Receive all active elements of list
-     * @param string $listKey
+     * Receive all elements of a list.
+     * Pagination of large lists is hidden inside this method
+     * @param string $relativeUrl
+     * @param array $requestData Request params
      * @param int $pageSize
      * @return array
      * @throws \Glsv\ElmaApi\exceptions\ElmaApiException
      * @throws \JsonException
      */
-    public function getAllItems(string $listKey, int $pageSize = 50): array
+    public function getAllItems(string $relativeUrl, array $requestData, int $pageSize = 100)
     {
         $items = [];
 
         $total = 1;
         $collected = 0;
 
-        $requestData = [
-            "active" => true,
-            "size" => $pageSize,
-            "sortExpressions" => [
-                [
-                    "ascending" => true,
-                    "field" => "__index"
-                ]
+        $requestData = array_merge(
+            [
+                "active" => true,
+                "size" => $pageSize,
+                "sortExpressions" => [
+                    [
+                        "ascending" => true,
+                        "field" => "__index"
+                    ]
+                ],
             ],
-            "from" => 0
-        ];
+            $requestData,
+            [
+                "from" => 0
+            ]
+        );
 
         while ($collected <= $total) {
-            $result = $this->api->makePost($listKey . '/list', $requestData);
+            $result = $this->api->makePost($relativeUrl, $requestData);
 
             $total = $result['result']['total'] ?? 0;
             $list = $result['result']['result'] ?? [];
